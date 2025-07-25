fff2f4be2a0540fc55b2db468e5d854734a34679
Naman Jain <njudaipur04@gmail.com>
2025-07-25 18:23:40 +0530

    12: agents data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,15 +1,21 @@
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
-    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    getOne: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input }) => {
         const [existingAgent] = await db
-            .select()
+            .select({
+                // TODO: Change to actual count
+                ...getTableColumns(agents),
+                meetingCount: sql<number>`5`,
+            })
             .from(agents)
             .where(eq(agents.id, input.id))
 
@@ -17,7 +23,11 @@ export const agentsRouter = createTRPCRouter({
     }),
     getMany: protectedProcedure.query(async () => {
         const data = await db
-            .select()
+            .select({
+                // TODO: Change to actual count
+                ...getTableColumns(agents),
+                meetingCount: sql<number>`5`,
+            })
             .from(agents);
 
         return data;
