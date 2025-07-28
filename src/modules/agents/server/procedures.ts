99a0a61d1c8bcf2e5be11b91ee9f7cf9b0524b30
Naman Jain <njudaipur04@gmail.com>
2025-07-28 09:23:17 +0530

    15: agent update and delete

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { agentsInsertSchema } from "../schemas";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schemas";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
 import { db } from "@/db";
@@ -10,6 +10,53 @@ import { TRPCError } from "@trpc/server";
 
 
 export const agentsRouter = createTRPCRouter({
+    update: protectedProcedure
+        .input(agentsUpdateSchema)
+        .mutation(async ({ ctx, input }) => {
+            const [updatedAgent] = await db
+                .update(agents)
+                .set(input)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            
+            if(!updatedAgent){
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found"
+                });
+            }
+
+            return updatedAgent;
+        }),
+
+    remove: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const [removedAgent] = await db
+                .delete(agents)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            
+            if(!removedAgent){
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Agent not found"
+                });
+            }
+
+            return removedAgent;
+        }),
+
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
         .query(async ({ input, ctx }) => {
